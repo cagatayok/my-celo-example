310002ebe6c31c9d868abd02c636044b16ad6c31
Çağatay Ok <okcagatay551@gmail.com>
2025-10-21 15:52:53 +0300

    Clean up deployment comments in 00_deploy_your_contract.ts
    Removed commented instructions for deploying contracts.

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -9,16 +9,8 @@ import { Contract } from "ethers";
  * @param hre HardhatRuntimeEnvironment object.
  */
 const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  /*
-    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
-
-    When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
-    should have sufficient balance to pay for the gas fees for contract creation.
-
-    You can generate a random account with `yarn generate` or `yarn account:import` to import your
-    existing PK which will fill DEPLOYER_PRIVATE_KEY_ENCRYPTED in the .env file (then used on hardhat.config.ts)
-    You can run the `yarn account` command to check your balance in every network.
-  */
+ 
+  
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
